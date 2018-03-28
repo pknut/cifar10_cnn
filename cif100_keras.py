77132b8800459635710b9809ee07b86938e1e648
Paulina Knut <paulina.knut@pascal01.intra.codilime.com>
2018-03-28 09:58:51 +0200

    Increase cnn

--- cif100_keras.py
+++ cif100_keras.py
@@ -1,14 +1,15 @@
 """
-log-loss training   = 1.726
-log-loss validation = 4.258
-accuracy training   = 0.533
-accuracy validation = 0.540
+log-loss training   = 1.444
+log-loss validation = 1.788
+accuracy training   = 0.595
+accuracy validation = 0.553
 """
 import keras
 from keras.datasets import cifar100
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten, GaussianNoise, Dropout
 from keras.layers import Conv2D, MaxPool2D, AlphaDropout, BatchNormalization
+from keras.callbacks import EarlyStopping
 from deepsense import neptune
 from neptune_call import NeptuneCallback
 
@@ -28,31 +29,38 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
 
-model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=x_train.shape[1:]))
-model.add(Conv2D(32, (1, 1), activation='relu'))
-model.add(BatchNormalization())
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=x_train.shape[1:]))
+model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPool2D())
-model.add(Dropout(0.25))
-
-model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-model.add(Conv2D(64, (1, 1), activation='relu'))
 model.add(BatchNormalization())
-model.add(MaxPool2D())
-model.add(Dropout(0.25))
+model.add(Dropout(0.5))
 
 model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-model.add(Conv2D(128, (1, 1), activation='relu'))
-model.add(BatchNormalization())
+model.add(Conv2D(128, (3, 3), activation='relu'))
 model.add(MaxPool2D())
-model.add(AlphaDropout(0.5))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(256, (3, 3), activation='relu'))
+model.add(MaxPool2D())
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(512, (4, 3), activation='relu', padding='same'))
+model.add(MaxPool2D())
+model.add(Dropout(0.5))
 
 model.add(Flatten())
-model.add(Dense(512, activation='relu'))
-model.add(AlphaDropout(0.5))
+model.add(Dense(1024, activation='relu'))
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
 model.add(Dense(num_classes))
 model.add(Activation('softmax'))
 
-model.compile(optimizer='adam',
+optimizer = keras.optimizers.Adam(lr=0.0005)
+
+model.compile(optimizer=optimizer,
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
@@ -60,9 +68,11 @@ print(model.summary())
 ctx.channel_send('n_layers', len(model.layers))
 ctx.channel_send('n_parameters', model.count_params())
 
+early_stopping = EarlyStopping(patience=5)
+
 model.fit(x_train, y_train,
           epochs=epochs,
           batch_size=128,
           validation_data=(x_test, y_test),
           verbose=2,
-          callbacks=[NeptuneCallback(x_test, y_test, images_per_epoch=20)])
+          callbacks=[NeptuneCallback(x_test, y_test, images_per_epoch=20), early_stopping])
